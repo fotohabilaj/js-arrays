960dff2013d9150d3e9da1d15ed13cced22d028c
fotohabilaj <fotohabilaj@gmail.com>
2026-02-04 16:08:26 +0100

    0203 0204 complete

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -16,15 +16,19 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = teachers.;
+const longNames = teachers;
 console.log(longNames);
 
 
 // 3. Rimuovi 'Ed' dall'array teachers
+const edLessTeachers= teachers.toSpliced(1,1)
+console.log(edLessTeachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.includes('Fabio');
+console.log(isFabioPresent);
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
