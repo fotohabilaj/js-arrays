b48383e4e6c36065f0e448136b73d13de7182dee
fotohabilaj <fotohabilaj@gmail.com>
2026-02-04 17:09:30 +0100

    0204 complete

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -31,16 +31,25 @@ for(let i=0; i<teachers.length; i++){
 // 3. Rimuovi 'Ed' dall'array teachers
 
 for(let i=0; i<teachers.length; i++){
-  const name = teachers[i];
-  if(name!='Ed'){
-    console.log(name);
+  const nome = teachers[i];
+  if(nome!='Ed'){
+    console.log(nome);
 
   }
 }
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+
+for(let i=0; i<teachers.length; i++){
+  const nome = teachers[i];
+  if(nome =='Fabio'){
+    isFabioPresent= true
+    console.log(isFabioPresent);
+
+  }
+}
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
